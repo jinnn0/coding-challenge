d7f1b88899656d35bca89d9e15644e9cad744a58
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-06-21 16:50:42 +0100

    Update 08. Remove Duplicates From Sorted Array.js

--- 08. Remove Duplicates From Sorted Array/08. Remove Duplicates From Sorted Array.js
+++ 08. Remove Duplicates From Sorted Array/08. Remove Duplicates From Sorted Array.js
@@ -19,6 +19,7 @@ const removeDuplicates = (nums) => {
 
 // Runtime: 100 ms, faster than 53.31% of JavaScript online submissions for Remove Duplicates from Sorted Array.
 // Memory Usage: 40.7 MB, less than 82.44% of JavaScript online submissions for Remove Duplicates from Sorted Array
+
 const removeDuplicates = (nums) => {
   for (let i = nums.length - 2; i >= 0; i--) {
     if (nums[i] === nums[i + 1]) {
@@ -26,4 +27,19 @@ const removeDuplicates = (nums) => {
     }
   }
   return nums.length;
+};
+
+// Runtime: 80 ms, faster than 98.22% of JavaScript online submissions for Remove Duplicates from Sorted Array.
+// Memory Usage: 41.4 MB, less than 15.15% of JavaScript online submissions for Remove Duplicates from Sorted Array.
+
+const removeDuplicates = (nums) => {
+  let index = 1;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[index++] = nums[i + 1];
+    }
+  }
+
+  return index;
 };
