170ea52b3560f29d30c889072cf2198fb868dca6
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-06-24 15:13:47 +0100

    Add 11. Search Insert Position.js

---
+++ 11. Search Insert Position/11. Search Insert Position.js
@@ -0,0 +1,70 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+/*
+ Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ If not, return the index where it would be if it were inserted in order.
+
+ You must write an algorithm with O(log n) runtime complexity.
+
+
+ nums = [1,3,5,6], target = 5   => output 2
+ nums = [1,3,5,6], target = 2   => output 1
+ nums = [1,3,5,6], target = 7   => output 4
+ nums = [1,3,5,6], target = 0   => output 0
+ nums = [0], target = 0         => output 0
+ */
+
+// Runtime: 68 ms, faster than 96.54% of JavaScript online submissions for Search Insert Position.
+// Memory Usage: 40 MB, less than 7.49% of JavaScript online submissions for Search Insert Position.
+const searchInsert = (nums, target) => {
+  const targetIndex = nums.indexOf(target);
+
+  if (0 < targetIndex) return targetIndex;
+  else {
+    let i = 0;
+    let index = 0;
+
+    while (i < nums.length) {
+      if (target > nums[i]) {
+        index++;
+      }
+      i++;
+    }
+
+    return index;
+  }
+};
+
+// Runtime: 76 ms, faster than 76.91% of JavaScript online submissions for Search Insert Position.
+// Memory Usage: 39.7 MB, less than 16.00% of JavaScript online submissions for Search Insert Position.
+const searchInsert = (nums, target) => {
+  const index = nums.indexOf(target);
+
+  if (0 < index) return index;
+  else {
+    let i = 0;
+
+    while (nums[i] < target) i++;
+    return i;
+  }
+};
+
+// Runtime: 76 ms, faster than 76.91% of JavaScript online submissions for Search Insert Position.
+// Memory Usage: 39.6 MB, less than 17.71% of JavaScript online submissions for Search Insert Position.
+const searchInsert = (nums, target) => {
+  let i = 0;
+
+  while (nums[i] < target) i++;
+  return i;
+};
+
+// Runtime: 76 ms, faster than 76.91% of JavaScript online submissions for Search Insert Position.
+// Memory Usage: 39.6 MB, less than 17.71% of JavaScript online submissions for Search Insert Position.
+const searchInsert = (nums, target) => {
+  const index = nums.findIndex((num) => num >= target);
+  return index === -1 ? nums.length : index;
+};
